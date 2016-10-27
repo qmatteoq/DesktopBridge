42726f816628eb32177d5b1a3599b210d372b962
Matteo Pagani <info@qmatteoq.com>
2016-10-27 22:15:32 +0200

    Added a new sample that demoes the container approach

---
+++ Extras/Container/DesktopApp.ReadRegistry/Form1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp.ReadRegistry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+private void OnReadRegistryKey(Object sender, EventArgs e)
+{
+    bool isAppInstalled = false;
+
+    var RegKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Matteo Pagani\DesktopBridge", true);
+    if (RegKey != null)
+    {
+        string result = RegKey.GetValue("IsAppInstalled").ToString();
+        isAppInstalled = result == "True" ? true : false;
+    }
+
+    string message = isAppInstalled ? "The app is installed" : "The app isn't installed";
+    MessageBox.Show(message);
+}
+    }
+}
---
+++ Extras/Container/DesktopApp.WriteRegistry/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp.WriteRegistry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCreateRegistryKey(Object sender, EventArgs e)
+        {
+            var RegKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Matteo Pagani\DesktopBridge", true);
+
+            if (RegKey == null)
+            {
+                RegKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Matteo Pagani\DesktopBridge", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                RegKey.SetValue("IsAppInstalled", true);
+            }
+        }
+
+        private void OnOpenReadApp(Object sender, EventArgs e)
+        {
+            Directory.GetCurrentDirectory();
+            string result = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            int index = result.LastIndexOf("\\");
+            string processPath = $"{result.Substring(0, index)}\\DesktopApp.ReadRegistry.exe";
+            Process.Start(processPath);
+        }
+    }
+}
