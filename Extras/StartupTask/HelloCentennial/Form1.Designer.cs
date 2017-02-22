b956be95a2fab73be766d34f9fb087903abccf24
Matteo Pagani <info@qmatteoq.com>
2017-02-22 15:48:04 +0100

    Added StartupTask example

---
+++ Extras/StartupTask/HelloCentennial/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace HelloCentennial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkBoxStartup = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create file on desktop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnCreateFile);
+            // 
+            // chkBoxStartup
+            // 
+            this.chkBoxStartup.AutoSize = true;
+            this.chkBoxStartup.Location = new System.Drawing.Point(216, 225);
+            this.chkBoxStartup.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chkBoxStartup.Name = "chkBoxStartup";
+            this.chkBoxStartup.Size = new System.Drawing.Size(130, 24);
+            this.chkBoxStartup.TabIndex = 1;
+            this.chkBoxStartup.Text = "Run at startup";
+            this.chkBoxStartup.UseVisualStyleBackColor = true;
+            this.chkBoxStartup.Click += new System.EventHandler(this.chkBoxStartup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 368);
+            this.Controls.Add(this.chkBoxStartup);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkBoxStartup;
+    }
+}
+
---
+++ Extras/StartupTask/HelloCentennial/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HelloCentennial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCreateFile(object sender, EventArgs e)
+        {
+            string userPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string fileName = $"{userPath}\\centennial.txt";
+            File.WriteAllText(fileName, "This file has been created by a Centennial app");
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var startupTask = await Windows.ApplicationModel.StartupTask.GetAsync("HelloCentennialTask");
+            switch (startupTask.State)
+            {
+                case Windows.ApplicationModel.StartupTaskState.Disabled:
+                    chkBoxStartup.Checked = false;
+                    break;
+                case Windows.ApplicationModel.StartupTaskState.DisabledByUser:
+                    chkBoxStartup.Checked = false;
+                    break;
+                case Windows.ApplicationModel.StartupTaskState.Enabled:
+                    chkBoxStartup.Checked = true;
+                    break;
+            }
+        }
+
+        private async void chkBoxStartup_Click(object sender, EventArgs e)
+        {
+            var startupTask = await Windows.ApplicationModel.StartupTask.GetAsync("HelloCentennialTask");
+            if (startupTask.State == Windows.ApplicationModel.StartupTaskState.Enabled)
+            {
+                startupTask.Disable();
+                chkBoxStartup.Checked = false;
+                MessageBox.Show("The task has been disabled");
+            }
+            else
+            {
+                var state = await startupTask.RequestEnableAsync();
+                switch (state)
+                {
+                    case Windows.ApplicationModel.StartupTaskState.DisabledByUser:
+                        MessageBox.Show("The task has been disabled by the user");
+                        chkBoxStartup.Checked = false;
+                        break;
+                    case Windows.ApplicationModel.StartupTaskState.Enabled:
+                        MessageBox.Show("The task has been enabled");
+                        chkBoxStartup.Checked = true;
+                        break;
+                }
+            }
+        }
+    }
+}
