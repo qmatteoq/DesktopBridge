fdcc41c48403137909452e8ac92d50921dac2688
Matteo Pagani <info@qmatteoq.com>
2017-03-07 19:58:49 +0100

    Added an icon to the main form and the app

--- 1. Desktop App Converter/HelloCentennial/HelloCentennial/Form1.Designer.cs
+++ 1. Desktop App Converter/HelloCentennial/HelloCentennial/Form1.Designer.cs
@@ -28,14 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(191, 105);
+            this.button1.Location = new System.Drawing.Point(127, 68);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(196, 62);
+            this.button1.Size = new System.Drawing.Size(131, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "Create file on desktop";
             this.button1.UseVisualStyleBackColor = true;
@@ -43,10 +45,12 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(573, 368);
+            this.ClientSize = new System.Drawing.Size(382, 239);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
